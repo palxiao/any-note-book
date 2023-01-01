9a22a0dd04aae09631539582c505a1ec47c069a4
ShawnPhang <palxiao@vip.qq.com>
2023-01-01 18:39:38 +0800

    fix: 文本替换错误

--- server/utils/generate.js
+++ server/utils/generate.js
@@ -1,11 +1,3 @@
-/*
- * @Author: ShawnPhang
- * @Date: 2022-07-27 23:45:30
- * @Description:
- * @LastEditors: ShawnPhang
- * @LastEditTime: 2022-08-03 10:17:41
- * @site: book.palxp.com
- */
 const fs = require('fs')
 const { getResourcesPath, getTemplatePath } = require('./index')
 
@@ -27,15 +19,15 @@ const generateData = {
 const tempGenerate = (params) => {
   try {
     let _index = fs.readFileSync(`${getTemplatePath()}/pending/_index`, 'utf8') // 读取模板文件
-    _index = _index.replace(/{{name}}/g, params.name)
-    _index = _index.replace(/{{repo}}/g, params.repo)
-    _index = _index.replace(/{{blog}}/g, generateData.blog(params.blog))
-    _index = _index.replace(/{{juejin}}/g, generateData.juejin(params.juejin))
+    _index = _index.replace(/{{ name }}/g, params.name)
+    _index = _index.replace(/{{ repo }}/g, params.repo)
+    _index = _index.replace(/{{ blog }}/g, generateData.blog(params.blog))
+    _index = _index.replace(/{{ juejin }}/g, generateData.juejin(params.juejin))
     let allPlugins = ''
     for (const plugin of params.plugin) {
       allPlugins += generateData[plugin]
     }
-    _index = _index.replace(/{{plugins}}/g, allPlugins)
+    _index = _index.replace(/{{ plugins }}/g, allPlugins)
     fs.writeFileSync(`${getResourcesPath()}/docs/index.html`, _index) // 写入目标文件
   } catch (err) {
     console.error(err)
